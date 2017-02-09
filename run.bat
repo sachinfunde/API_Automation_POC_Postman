5614190499bf660fdb662375dbf9d99da60e1b05
sachinfunde <sachinfunde889@gmail.com>
2017-02-09 18:26:19 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,1 +1,2 @@
+cd C:\Users\jfundes\.jenkins\workspace\Jenkins_Git_integration
 newman run MEMS_Webservice_automation.postman_collection.json -e MEMS.postman_environment.json -d testData.json -n 1 --insecure --reporters html
